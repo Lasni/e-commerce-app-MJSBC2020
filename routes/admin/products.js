9f2593e4e41fed79dc46b6b61b3c245ec3e0180e
grega <grega.lasnibat@gmail.com>
2020-11-27 17:34:05 +0100

    edit product validation and error handling

--- routes/admin/products.js
+++ routes/admin/products.js
@@ -52,7 +52,10 @@ router.post(
   requireAuth,
   upload.single("image"),
   [requireTitle, requirePrice],
-  handleErrors(productsEditTemplate),
+  handleErrors(productsEditTemplate, async (req) => {
+    const product = await productsRepo.getOne(req.params.id);
+    return { product };
+  }),
   async (req, res) => {
     const changes = req.body;
 
