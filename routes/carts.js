e12e48d996d713220a52e0055a863a7d34c251a7
grega <grega.lasnibat@gmail.com>
2020-11-30 11:20:49 +0100

    deleting products

--- routes/carts.js
+++ routes/carts.js
@@ -1,5 +1,8 @@
 const express = require("express");
+
 const cartsRepo = require("../repositories/carts");
+const productsRepo = require("../repositories/products");
+const cartShowTemplate = require("../views/carts/show");
 
 const router = express.Router();
 
@@ -29,13 +32,42 @@ router.post("/cart/products", async (req, res) => {
   await cartsRepo.update(cart.id, {
     items: cart.items,
   });
-
-  console.log(cart);
-  res.send("product added to cart");
+  res.redirect("/cart");
 });
 
 // show all items in cart GET
+router.get("/cart", async (req, res) => {
+  // if no cart created, redirect back to home page
+  if (!req.session.cartId) {
+    return res.redirect("/");
+  }
+
+  const cart = await cartsRepo.getOne(req.session.cartId);
+
+  for (let item of cart.items) {
+    // item === {id, quantity}
+    const product = await productsRepo.getOne(item.id);
+    item.product = product; // just assigning product obj to item.product for further templating
+  }
+  res.send(cartShowTemplate({ items: cart.items }));
+});
 
 // delete an item from cart POST
+router.post("/cart/products/delete", async (req, res) => {
+  const { itemId } = req.body;
+  let cart;
+  cart = await cartsRepo.getOne(req.session.cartId);
+
+  const items = cart.items.filter((item) => item.id !== itemId);
+  await cartsRepo.update(req.session.cartId, { items });
+
+  // if deleted last item, redirect to products
+  cart = await cartsRepo.getOne(req.session.cartId);
+  if (cart.items.length === 0) {
+    return res.redirect("/");
+  }
+
+  res.redirect("/cart");
+});
 
 module.exports = router;
